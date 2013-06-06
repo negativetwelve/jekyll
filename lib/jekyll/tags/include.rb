d8d9ce0a5f04a36eb874753d6b04d1d08e5da55c
Mark Miyashita <negativetwelve@gmail.com>
2013-06-05 20:33:31 -0700

    Fixing placement of contect var

--- lib/jekyll/tags/include.rb
+++ lib/jekyll/tags/include.rb
@@ -4,30 +4,30 @@ module Jekyll
       def initialize(tag_name, file, tokens)
         super
         @file = file.strip
-        @file = context[@file] || @file
       end
 
       def render(context)
         includes_dir = File.join(context.registers[:site].source, '_includes')
+        file = context[@file] || @file
 
         if File.symlink?(includes_dir)
           return "Includes directory '#{includes_dir}' cannot be a symlink"
         end
 
-        if @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
-          return "Include file '#{@file}' contains invalid characters or sequences"
+        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
+          return "Include file '#{file}' contains invalid characters or sequences"
         end
 
         Dir.chdir(includes_dir) do
           choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(@file)
-            source = File.read(@file)
+          if choices.include?(file)
+            source = File.read(file)
             partial = Liquid::Template.parse(source)
             context.stack do
               partial.render(context)
             end
           else
-            "Included file '#{@file}' not found in _includes directory"
+            "Included file '#{file}' not found in _includes directory"
           end
         end
       end
